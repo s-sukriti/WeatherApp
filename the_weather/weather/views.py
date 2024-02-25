7fdbbb7c1da8edec35660b13719012486ec7deec
Sukriti S <eng21ct0039@dsu.edu.in>
2024-02-25 19:31:23 +0530

    changes made

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -5,29 +5,41 @@ from .forms import CityForm
 
 def index(request):
     api_key = 'a5b0751817608ebcbbe1550b9603f114'
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
-    
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + api_key
+
     cities = City.objects.all()
+    form = CityForm(request.POST)  # Move the form instantiation outside of the if statement
+
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
-    form = CityForm()
+        if form.is_valid():
+            city_name = form.cleaned_data['name']
 
+            # Check if the city exists by making an API request
+            city_weather = requests.get(url.format(city_name)).json()
+
+            if 'main' in city_weather:
+                # City exists, proceed to save to database
+                form.save()
+            else:
+                # City doesn't exist or API response is invalid
+                form.add_error('name', f"Error: City '{city_name}' doesn't exist or API response is invalid.")
+        
     weather_data = []
 
     for city in cities:
-        city_name = city.name
-        city_weather = requests.get(url.format(city_name, api_key)).json()
+        city_weather = requests.get(url.format(city)).json()
 
-        weather = {
-            'city': city_name,
-            'temperature': city_weather['main']['temp'],
-            'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon']
-        }
-
-        weather_data.append(weather)
+        if 'main' in city_weather:
+            weather = {
+                'city': city,
+                'temperature': city_weather['main']['temp'],
+                'description': city_weather['weather'][0]['description'],
+                'icon': city_weather['weather'][0]['icon']
+            }
+            weather_data.append(weather)
+        else:
+            print(f"Skipping {city} due to missing 'main' key in API response")
 
     context = {'weather_data': weather_data, 'form': form}
-
     return render(request, 'weather/index.html', context)
